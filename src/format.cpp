05113865cf4374cff94da996a9088ab39a4076a3
Riccardo <riccardo@null.net>
2020-01-13 12:04:15 +0100

    add ElapsedTime function for UpTime calculation

--- src/format.cpp
+++ src/format.cpp
@@ -7,5 +7,16 @@ using std::string;
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  long hours, minutes;
+  string hr, min ,sec;
+  minutes = seconds / 60;
+  seconds = seconds % 60;
+  sec = (seconds < 10) ? ("0" + std::to_string(seconds)) : std::to_string(seconds);
+  hours = minutes / 60;
+  minutes = minutes % 60;
+  min = (minutes < 10) ? ("0" + std::to_string(minutes)) : std::to_string(minutes);
+  hr = (hours < 10) ? ("0" + std::to_string(hours)) : std::to_string(hours);
+
+  return hr + ":" + min + ":" + sec;
+}
